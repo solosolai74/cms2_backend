9297409ad0539b6b1c21a99981fd0d465356d428
boss <solairajs@cdac.in>
2024-10-22 14:29:16 +0530

    choices.py file utils added

---
+++ utils/choices.py
@@ -0,0 +1,11 @@
+center_status = (("ACTIVE","ACTIVE"),
+("WITHHELD","WITHHELD"))
+
+center_rating = (("1","1"),
+("2","2"),
+("3","3"),
+("4","4"),
+("5","5"),)
+
+member_type = (("CDAC","CDAC"),
+("NON-CDAC","NON-CDAC"))
