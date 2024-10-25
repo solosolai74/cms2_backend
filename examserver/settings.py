06f62fd502ebc907c37310154d78cf3d1682866f
Siddhant <siddhantn@cdac.in>
2024-10-25 23:20:49 +0530

    CORS header middlware added

--- examserver/settings.py
+++ examserver/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'apicalls',
     'exam'
 ]
@@ -49,7 +50,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+
+CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_WHITELIST = (
+#        'http://localhost:4200',
+# )
 
 ROOT_URLCONF = 'examserver.urls'
 
